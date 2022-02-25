1e902e06adc865ffedc8b05705b190bf40351b23
Dumukenova Aitunuk <dumukenova05@gmail.com>
2022-02-25 15:49:26 +0600

    Added style

---
+++ script.js
@@ -0,0 +1,57 @@
+// document.querySelector('select').addEventListener('input', function() {
+//     const value = this.value;
+// document.querySelectorAll('ul').forEach(function(ul){
+//     if(value === ul.id){
+//         ul.className =''
+//     }
+//     else{
+//         ul.className = 'hidden'
+//     }
+// });
+// });
+
+
+
+// const alticles = [
+//     {
+//         id: "article-1",
+//         title:"hello world",
+//         image: "images/images.jpg",
+//         body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolorem iste porro provident ea ad doloremque dicta soluta consequatur cupiditate deserunt dolor similique iusto dolorum, necessitatibus delectus officiis, labore laborum"
+       
+//     },
+//     {
+//         id: "article-1",
+//         title:"hello world",
+//         image: "images/images.jpg",
+//         body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolorem iste porro provident ea ad doloremque dicta soluta consequatur cupiditate deserunt dolor similique iusto dolorum, necessitatibus delectus officiis, labore laborum"
+       
+//     },
+//     {
+//         id: "article-1",
+//         title:"hello world",
+//         image: "images/images.jpg",
+//         body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolorem iste porro provident ea ad doloremque dicta soluta consequatur cupiditate deserunt dolor similique iusto dolorum, necessitatibus delectus officiis, labore laborum"
+       
+//     }
+    
+// ]
+
+
+
+let details = document.querySelectorAll('details');
+details.forEach((detail))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
